980e74c6126d2247ba194703253f61b6fea5c8fd
unknown <mdzulfahmishoib@gmail.com>
2024-11-15 17:07:45 +0700

    first commit

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $ownerRole = Role::create([
+            'name' => 'owner'
+        ]);
+        
+        $buyerRole = Role::create([
+            'name' => 'buyer'
+        ]);
+        
+        $user = User::create([
+            'name' => 'Fany Pemilik',
+            'email' => 'fany@owner.com',
+            'password' => bcrypt('admin'),
+        ]);
+
+        $user->assignRole($ownerRole);
+    }
+}
